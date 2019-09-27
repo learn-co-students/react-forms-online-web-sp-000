6ed808852cde4a55619044e9bc9b5bc9a9fb5c2f
Diana Badas <diana.badas@gmail.com>
2019-09-27 14:51:25 +0100

    Done.

--- src/components/ControlledInput.js
+++ src/components/ControlledInput.js
@@ -3,21 +3,23 @@ import React from 'react';
 
 class ControlledInput extends React.Component {
     state = {
-        value: 'ola'
+        olakase: 'ola'
     }
 
     handleChange = event => {
         this.setState({
             [event.target.name]: event.target.value
         });
+        console.log(this.state.olakease)
     }
 
     render() {
         return (
-            <form onSubmit={event=> this.handleSubmit(event)}>
+            <form onSubmit={event => this.handleSubmit(event)}>
               <input 
               type="text"
-              value={this.state.value}
+              className="olakase"
+              value={this.state.olakase}
               onChange={this.handleChange}
             />
             </form>
--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import ControlledInput from './components/ControlledInput';
+import ControlledInput from './components/ControlledInput'
 
 ReactDOM.render(
-  <div>Mount Components Here
-    <ControlledInput />
-  </div>,
+  <ControlledInput/>,
   document.getElementById('root')
 );

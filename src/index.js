17f55d040694e5c4a430b4bf6f3ff00e71600a46
AlyssaYesilyurt <alyssa.yesilyurt@gmail.com>
2020-06-15 15:37:57 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Form from './components/Form'
-
-ReactDOM.render(
-  <div>
-    <Form />
-  </div>,
-  document.getElementById('root')
-);
+    // src/index.js
+    import React from 'react';
+    import ReactDOM from 'react-dom';
+    import ParentComponent from './components/ParentComponent';
+     
+    ReactDOM.render(
+      <div>
+        <ParentComponent/>
+      </div>,
+      document.getElementById('root')
+    );

f7893f4000941cc99ce46756080c07948647ca6a
VesnaVucinic <vesna.vucinic68@gmail.com>
2020-10-01 18:47:41 +0100

    Done.

---
+++ build/precache-manifest.0c100c108bd31052af875eafe87416f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1a2935f3aa0319c0d8cb7a934f79ca8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "927fbfa44be98a9afa1c",
+    "url": "/static/js/2.97b4407a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.97b4407a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "390cef57c765de03e512",
+    "url": "/static/js/main.52172858.chunk.js"
+  },
+  {
+    "revision": "b6b7d9f341d802461e6f",
+    "url": "/static/js/runtime-main.b2d6c529.js"
+  }
+]);
